6457d75655f2c8d1f94764f09ed14f3da68e73c6
XenonTheCoder-bloxd <299334463+XenonTheCoder-bloxd@users.noreply.github.com>
2026-09-05 17:26:19 -0700

    Add avatar properties to user query

--- functions/api/users/directory.js
+++ functions/api/users/directory.js
@@ -1,6 +1,6 @@
 export async function onRequestGet({ env }) {
   const { results } = await env.DB.prepare(
-    `SELECT username, bio, avatar, xp, lessons, profile_views
+    `SELECT username, bio, avatar, avatar_zoom, avatar_pos_x, avatar_pos_y, xp, lessons, profile_views
      FROM users ORDER BY xp DESC LIMIT 200`
   ).all();
 
